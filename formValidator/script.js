0dfd64181b7e059baedc9458e0dab39a1a7125e8
timashan <kavishkatimashan@gmail.com>
2022-03-10 02:02:03 +0530

    movieSeatBooking

---
+++ formValidator/script.js
@@ -0,0 +1,61 @@
+const form = document.querySelector(".form");
+const btnSubmit = document.querySelector(".btn--submit");
+const inputs = document.querySelectorAll(".form-control input");
+const errorELs = document.querySelectorAll(".form-control small");
+
+const capitalize = (text) => {
+  return text[0].toUpperCase() + text.slice(1);
+};
+
+const showSucceed = function (input) {
+  input.parentElement.classList.remove("error");
+  input.parentElement.classList.add("success");
+};
+
+const showError = function (input, text) {
+  input.parentElement.classList.add("error");
+  input.nextElementSibling.innerText = text;
+};
+
+const checkEmail = function (input) {
+  const regex = /\S+@\S+\.\S+/;
+  const ok = regex.test(input.value.toLowerCase().trim());
+  if (ok) showSucceed(input);
+  if (!ok) showError(input, `${capitalize(input.id)} is invalid`);
+};
+
+const checkEmpty = function () {
+  inputs.forEach((input) => {
+    const ok = input.value.trim() !== "";
+    if (ok) showSucceed(input);
+    if (!ok) showError(input, `${capitalize(input.id)} is required`);
+  });
+};
+
+const checkLength = function (input, min, max) {
+  const length = input.value.length;
+  if (length > max)
+    showError(
+      input,
+      `${capitalize(input.id)} cannot be below ${max} characthers`
+    );
+  if (length < min)
+    showError(
+      input,
+      `${capitalize(input.id)} must be at least ${min} characthers`
+    );
+};
+
+const checkMatch = function (input1, input2) {
+  const ok = input1.value === input2.value;
+  if (!ok) showError(input2, "Passwords do not match");
+};
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  checkEmpty();
+  checkEmail(inputs[1]);
+  checkLength(inputs[0], 3, 15);
+  checkLength(inputs[2], 6, 25);
+  checkMatch(inputs[2], inputs[3]);
+});
---
+++ movieSeatBooking/script.js
@@ -0,0 +1,41 @@
+const container = document.querySelector(".container");
+const text = document.querySelector(".text");
+const select = document.querySelector(".select");
+const seats = container.getElementsByClassName("seat");
+const selectedSeats = container.getElementsByClassName("selected");
+
+const updateText = function () {
+  text.children[0].innerText = selectedSeats.length;
+  text.children[1].innerText = select.value * selectedSeats.length;
+};
+
+const populateUI = function () {
+  const data = JSON.parse(localStorage.getItem("seats"));
+  if (!data) return;
+  console.log(data);
+  data.idxs.forEach((i) => seats[i].classList.add("selected"));
+  select.selectedIndex = data.select;
+  updateText();
+};
+populateUI();
+
+const saveSeats = function () {
+  const idxs = [...selectedSeats].map((s, idx) => [...seats].indexOf(s));
+  localStorage.setItem(
+    "seats",
+    JSON.stringify({ idxs: idxs, select: select.selectedIndex })
+  );
+};
+
+container.addEventListener("click", function (e) {
+  const seat = e.target.closest(".seat:not(.occupied)");
+  if (!seat) return;
+  seat.classList.toggle("selected");
+  updateText();
+  saveSeats();
+});
+
+select.addEventListener("change", function () {
+  updateText();
+  saveSeats();
+});
